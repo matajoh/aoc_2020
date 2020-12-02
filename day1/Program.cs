f45cd6205e7b38b89611310d19fdc9cf4ea580e3
Matthew Johnson <matjoh@microsoft.com>
2020-12-02 11:26:34 +0000

    Day 2

--- day1/Program.cs
+++ day1/Program.cs
@@ -6,58 +6,72 @@ namespace day1
 {
     class Program
     {
-        static Tuple<int, int> part1(int[] values)
+        const int VALUE = 2020;
+
+        static void part1(int[] values)
         {
-            for(int i=0; i<values.Length; ++i)
+            int right = values.Length - 1;
+            for(int left=0; left < values.Length; ++left)
             {
-                for(int j=i + 1; j < values.Length; ++j)
+                int other = VALUE - values[left];
+                while(values[right] > other)
                 {
-                    if(values[i] + values[j] == 2020)
-                    {
-                        return new Tuple<int, int>(values[i], values[j]);
-                    }
+                    --right;
+                }
+
+                if(values[right] == other)
+                {
+                    Console.WriteLine("Part 1: {0}", values[left] * values[right]);
+                    return;
                 }
             }
-            
-            throw new Exception("Pair not found");
+
+            throw new Exception("Pair not found.");
         }
 
-        static Tuple<int, int, int> part2(int[] values)
+        static void part2(int[] values)
         {
-            for(int i=0; i<values.Length; ++i)
+            int rightStart = values.Length - 1;
+            for(int left=0; left < values.Length; ++left)
             {
-                int value0 = values[i];
-                int value0_2 = value0 + value0;
-                for(int j=i; j<values.Length; ++j)
+                int remainder = VALUE - values[left];
+                while(values[rightStart] >= remainder)
                 {
-                    int value1 = values[j];
-                    int value01 = value0 + value1;
-                    if(value01 + value0 == 2020)
+                    --rightStart;
+                }
+
+                int right = rightStart;
+
+                for(int mid=left; mid < values.Length; ++mid)
+                {
+                    remainder = VALUE - values[left] - values[mid];
+                    if(remainder < values[left])
                     {
-                        return new Tuple<int, int, int>(value0, value0, value1);
+                        break;
                     }
 
-                    for(int k=j; k < values.Length; ++k)
+                    while(values[right] > remainder)
                     {
-                        int value2 = values[k];
-                        if(value01 + value2 == 2020)
-                        {
-                            return new Tuple<int, int, int>(value0, value1, value2);
-                        }
+                        --right;
+                    }
+
+                    if(values[right] == remainder)
+                    {
+                        Console.Write("Part 2: {0}", values[left] * values[mid] * values[right]);
+                        return;
                     }
                 }
             }
 
-            throw new Exception("Triplet not found");
+            throw new Exception("Triplet not found.");
         }
 
         static void Main(string[] args)
         {
             int[] values = File.ReadLines(args[0]).Select(s => int.Parse(s.Trim())).ToArray();
-            var pair = part1(values);
-            Console.WriteLine("Part 1: {0}", pair.Item1 * pair.Item2);
-            var triplet = part2(values);
-            Console.WriteLine("Part 2: {0}", triplet.Item1 * triplet.Item2 * triplet.Item3);
+            Array.Sort(values);
+            part1(values);
+            part2(values);
         }
     }
 }
---
+++ day2/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Linq;
+
+namespace day2
+{
+    enum RuleMode {
+        Part1,
+        Part2
+    }
+
+    class Password
+    {
+        private Password(int value0, int value1, char c, string password)
+        {
+            this._value0 = value0;
+            this._value1 = value1;
+            this._c = c;
+            this._password = password;
+        }
+
+        bool _part1Rule()
+        {
+            int count = this._password.Count(x => x == this._c);
+            return count >= this._value0 && count <= this._value1;
+        }
+
+        bool _part2Rule()
+        {
+            bool pos0 = this._password[this._value0 - 1] == this._c;
+            bool pos1 = this._password[this._value1 - 1] == this._c;
+            return pos0 ^ pos1;
+        }
+
+        public bool IsValid(RuleMode mode)
+        {
+            switch(mode)
+            {
+                case RuleMode.Part1:
+                    return this._part1Rule();
+                
+                case RuleMode.Part2:
+                    return this._part2Rule();
+                
+                default:
+                    throw new NotSupportedException();
+            }
+        }
+
+        public static Password Parse(string line)
+        {
+            string[] parts = line.Trim().Split();
+            int[] min_max = parts[0].Split('-').Select(x => int.Parse(x)).ToArray();
+            char c = parts[1][0];
+            return new Password(min_max[0], min_max[1], c, parts[2]);
+        }
+
+        private int _value0;
+        private int _value1;
+        private char _c;
+        private string _password;
+    }
+    class Program
+    {
+        static bool isValid1(int min, int max, char c, string password)
+        {
+            int count = password.Count(x => x == c);
+            return count >= min && count <= max;
+        }
+        static void part1(string[] lines)
+        {
+            int numValid = 0;
+            foreach(string line in lines)
+            {
+                string[] parts = line.Trim().Split();
+                int[] min_max = parts[0].Split('-').Select(x => int.Parse(x)).ToArray();
+                char c = parts[1][0];
+                if(isValid1(min_max[0], min_max[1], c, parts[2]))
+                {
+                    numValid += 1;
+                }
+            }
+
+            Console.WriteLine("Part 1: {0}", numValid);
+        }
+
+        static void Main(string[] args)
+        {
+            var lines = File.ReadLines(args[0]).ToList();
+            var passwords = lines.Select(line => Password.Parse(line));
+            Console.WriteLine("Part 1: {0}", passwords.Count(password => password.IsValid(RuleMode.Part1)));
+            Console.WriteLine("Part 2: {0}", passwords.Count(password => password.IsValid(RuleMode.Part2)));            
+        }
+    }
+}
